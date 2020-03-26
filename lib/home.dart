b0eb959cf1a56ca2196926df39101b08600a5fbb
Arib Dhuka <atxarib99@gmail.com>
2020-03-25 23:58:52 -0500

    Added bug fix for random navbar image

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'dart:math';
 
+import 'package:GrandExchangeMonitor/Item.dart';
 import 'package:GrandExchangeMonitor/NavDrawer.dart';
 import 'package:GrandExchangeMonitor/PageInterface.dart';
 import 'package:GrandExchangeMonitor/pagenum.dart';
@@ -7,6 +9,7 @@ import 'package:GrandExchangeMonitor/searchpage.dart';
 import 'package:GrandExchangeMonitor/watchlist.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:http/http.dart';
 class Home extends StatefulWidget {
 
   @override
@@ -24,21 +27,45 @@ class HomePageState extends State<Home> implements PageInterface {
 
   //holds url for random image
   String _url = 'http://services.runescape.com/m=itemdb_oldschool/1582802986184_obj_big.gif?id=13190';
-
+  
+  //url data to prevent repetitive code
+  final String _BASE_URL = "http://services.runescape.com/m=itemdb_oldschool";
+  final String _BASIC_APPEND = "/api/catalogue/detail.json?item=";
+  
   //load id to name map
   Future<String> loadAsset() async {
     //for some reason if you just do assets: assets/ this function does not work.
     return await rootBundle.loadString('assets/dict/IDtoItemName.csv');
   }
+
+  //searches for an item based on id
+  Future<Response> searchItem(String search) {
+    //construct url to search from
+    String finalURL = _BASE_URL + _BASIC_APPEND + search;
+    // return the future object that should hold a response from server
+    return get(
+      finalURL,
+      // Send authorization headers to the backend.
+      headers: {"Access-Control-Allow-Origin": "*"},
+    );
+  }
+
   
   void getRandomItemImage() async {
     Random rand = new Random();
-    int randomNum = rand.nextInt(3011);
+    int randomNum = rand.nextInt(3011); 
     //loads the item to id map
     loadAsset().then((value) {
       //split by lines
       List<String> lines = value.split("\n");
-      _url = 'http://services.runescape.com/m=itemdb_oldschool/1582802986184_obj_big.gif?id=' + lines[randomNum].split(",")[0];
+      //call the search by id method, then for the response
+      searchItem(lines[randomNum].split(",")[0]).then((res) {
+        //convert to a map from the JSON response
+        Map<String, dynamic> body = json.decode(res.body);
+        //set the item to the item generated from the JSON
+        Item item = Item.fromJSON(body);
+        _url = item.imageURL;
+      });
     });
   }
 
